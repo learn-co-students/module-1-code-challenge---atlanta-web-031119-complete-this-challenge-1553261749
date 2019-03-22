1da03b8530ca3448b4721d3fb003a1b880d00102
Darcy Linde <lindedarcy@gmail.com>
2019-03-22 10:59:08 -0400

    all done and working

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,50 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.all_names
+    self.all.map {|c| c.full_name}
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    Review.all.select {|r| self == r.customer}.size
+  end
+
+  def restaurants
+    reviews = Review.all.select {|r| self == r.customer}
+    reviews.map {|r| r.restaurant}.uniq
+  end
+
+  def self.find_by_name(name)
+    name_array = name.split(" ")
+    first = name_array[0]
+    last = name_array[1]
+    Review.all.find do |r|
+      r.customer.first_name == first && r.customer.last_name == last
+    end.customer
+  end
+
+  def self.find_all_by_first_name(name)
+    reviews = Review.all.select {|r| r.customer.first_name == name}
+    reviews.map {|r| r.customer}.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,17 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+darcy = Customer.new("darcy", "linde")
+brit = Customer.new("brit", "butler")
+darcy_2 = Customer.new("darcy", "imposter")
+pizza = Restaurant.new("Dominos")
+sushi = Restaurant.new("Wasabi_House")
+bar = Restaurant.new("Tavern")
 
+review_1 = Review.new(darcy, pizza, 4, "Not Bad!")
+review_2 = Review.new(brit, pizza, 1, "No French Onion Soup :(")
+darcy.add_review(sushi, "Yum!", 5)
+darcy.add_review(pizza, "Yuk!", 1)
+darcy_2.add_review(bar, "Never going back here", 5)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
