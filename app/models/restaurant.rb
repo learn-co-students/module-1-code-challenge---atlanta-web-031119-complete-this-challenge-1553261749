a3520ea6cab84140fd612f4f4f8a19dfe4780cfb
Samidh Patel <spatel1345@gmail.com>
2019-03-22 10:56:04 -0400

    finished code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
+  
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def customers
+    Review.all.select {|rev| rev.restaurant == self}.map {|rev| rev.customer}.uniq
+  end
+
+  def reviews
+    Review.all.select {|rev| rev.restaurant == self}.map {|rev| rev.content}.uniq
+  end 
+
+  def self.find_by_name(name)
+    @@all.find {|rest| rest.name == name}
+  end 
 
 end
