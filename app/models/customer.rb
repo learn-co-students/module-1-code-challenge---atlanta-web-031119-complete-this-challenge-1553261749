a904cc97378469ac2a46df82f848d8ad9ba51f2d
Ansara Hollinger <ansara.hooks@gmail.com>
2019-03-22 11:05:19 -0400

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,32 @@
 class Customer
+
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review (restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def restaurants
+    Review.all.select {|review| review.customer == self}.map {|review| review.restaurant}
+  end
+
+  def num_reviews
+    Review.all.select {|review| review.customer == self}.map {|review| review.length}
+  end
+
 end
