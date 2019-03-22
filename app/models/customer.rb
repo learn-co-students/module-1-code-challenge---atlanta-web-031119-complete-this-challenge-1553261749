d85e0b9af509b90767e7a78d1d5bbdd188c1c22c
github username <github email address>
2019-03-22 11:13:14 -0400

    Mode 1 Exam done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,52 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@customers = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@customers << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(name)
+    first_last = name.split
+    Customer.all.find do |customer|
+      first_last[0] == customer.first_name && first_last[1] == customer.last_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.select {|customer| customer.first_name == name }
+  end
+
+  def self.all_names
+    Customer.all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def my_reviews
+    Review.all.select {|review| review.customer == self }
+  end
+
+  def num_reviews
+    my_reviews.count
+  end
+
+  def restaurants
+    my_reviews.map {|review| review.restaurant.name }.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+john = Customer.new("John","Cho")
+john2 = Customer.new("John","Choi")
+hayeong = Customer.new("Hayeong","Noh")
+
+chipotle = Restaurant.new("Chipotle")
+optimist = Restaurant.new("The Optimist")
+
+john.add_review(optimist,1,"Too expensive for it to taste this bad!")
+john.add_review(chipotle,4,"I can have their burrito bowls everyday. It is so good.")
+john.add_review(chipotle,2,"They didn't give me enough chicken today...")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
